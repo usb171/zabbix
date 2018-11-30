da7ddb367c97597cb4673a4c93952d331b4c3017
Nilton <usb171@gmail.com>
2018-11-30 14:02:35 -0300

    Criado script python comn parâmetros

--- zabbixMqtt.py
+++ zabbixMqtt.py
@@ -1,21 +1,3 @@
-'''import subprocess
-from ZabbixSender import ZabbixSender, ZabbixPacket
-
-_HOST = "SENSORES"
-_PORT_ZABBIX = 10051
-_SERVER_ZABBIX = "zabbixcv.ddns.net"
-
-server = ZabbixSender(_SERVER_ZABBIX, _PORT_ZABBIX)
-packet = ZabbixPacket()
-
-temp = float(subprocess.Popen("mosquitto_sub -h srcv.ddns.net -p 9999 -t 002/temp -C 1", shell=True, stdout=subprocess.PIPE).stdout.read().decode())
-humi = float(subprocess.Popen("mosquitto_sub -h srcv.ddns.net -p 9999 -t 002/humi -C 1", shell=True, stdout=subprocess.PIPE).stdout.read().decode())
-packet.add(_HOST,'temp', temp)
-packet.add(_HOST,'humi', humi)
-server.send(packet)
-print(server.status)'''
-
-
 import sys
 import subprocess
 from ZabbixSender import ZabbixSender, ZabbixPacket
